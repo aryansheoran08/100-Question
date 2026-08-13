9bca13bbb1de8d8df74254b1f9dd63354cc76e0e
aryansheoran08 <aryansheoran208@gmail.com>
2026-08-13 21:28:13 +0530

    Add files via upload

---
+++ Q-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    int a;
+    int b;
+    printf ("enter the first number a: ");
+    scanf ("%d",&a);
+    printf ("enter the second number b: ");
+    scanf ("%d",&b);
+    printf ("sum of both the number : %d", a+b);
+    return 0;
+
+}
