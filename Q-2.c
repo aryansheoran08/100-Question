1883764994fb8d6f539badd82407ea330890903b
aryansheoran08 <aryansheoran208@gmail.com>
2026-08-13 21:32:45 +0530

    Add files via upload

---
+++ Q-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    printf ("enter the first number a : ");
+    scanf ("%d",&a);
+    printf ("enter the second number b : ");
+    scanf ("%d",&b);
+    printf ("sum of both number : %d\n",a+b);
+    printf ("difference of both number : %d\n ",a-b);
+    printf ("multiplication of both number : %d\n",a*b);
+    printf ("division of both number : %d",a/b);
+    return 0 ;
+
+}
