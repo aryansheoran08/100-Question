24d13be57e5ed94b9005b5342e5f594daed400ae
aryansheoran08 <aryansheoran208@gmail.com>
2026-08-15 16:10:18 +0530

    Add files via upload

---
+++ Q-8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf ("enter the value of n : ");
+    scanf ("%d",&n);
+    printf ("Sum of n natural number : %d ", n*(n+1)/2);
+    return 0 ;
+}
+
