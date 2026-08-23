3ef56873d85d78c39a92d3446444736625599199
aryansheoran08 <aryansheoran208@gmail.com>
2026-08-23 16:48:01 +0530

    Add files via upload

---
+++ Q-27.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    printf("Sum of n odd number : %d",n*n);
+    return 0;
+    
+}
---
+++ Q-28.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int n:
+    printf("Enter the value of n : ")
+    scanf("%d",&n);
+    printf("product of even number : %d",2^(n/2)*n!);
+    return 0;
+    
+}
