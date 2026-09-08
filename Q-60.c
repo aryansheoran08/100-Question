e8ed5d8a8d6d0ef49cc0e5fbf7766c6220bd50dd
aryansheoran08 <aryansheoran208@gmail.com>
2026-09-08 18:19:35 +0530

    Add files via upload

---
+++ Q-60.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+    int positive = 0, negative = 0, zero = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] > 0)
+            positive++;
+        else if (arr[i] < 0)
+            negative++;
+        else
+            zero++;
+    }
+
+    printf("Number of positive elements = %d\n", positive);
+    printf("Number of negative elements = %d\n", negative);
+    printf("Number of zero elements = %d\n", zero);
+
+    return 0;
+}
