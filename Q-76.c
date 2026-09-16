2c0c223ae07f18daba172702db9ce6089e9cf5f0
aryansheoran08 <aryansheoran208@gmail.com>
2026-09-16 19:06:23 +0530

    Add files via upload

---
+++ Q-76.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], n, i, j;
+    int symmetric = 1;
+
+    printf("Enter the order of matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter elements of matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Check for symmetry
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (a[i][j] != a[j][i]) {
+                symmetric = 0;
+                break;
+            }
+        }
+    }
+
+    if (symmetric)
+        printf("The matrix is symmetric.\n");
+    else
+        printf("The matrix is not symmetric.\n");
+
+    return 0;
+}
