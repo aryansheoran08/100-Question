59d44570daa666ec0686850d91a4409631d1b673
aryansheoran08 <aryansheoran208@gmail.com>
2026-09-21 16:18:19 +0530

    Add files via upload

---
+++ Q-86.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, length, palindrome = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    length = strlen(str);
+
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - 1 - i]) {
+            palindrome = 0;
+            break;
+        }
+    }
+
+    if (palindrome)
+        printf("The string is a palindrome.");
+    else
+        printf("The string is not a palindrome.");
+
+    return 0;
+}
