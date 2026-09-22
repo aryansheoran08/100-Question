fda95db917af7148b1400524b85554fbc9fccc89
aryansheoran08 <aryansheoran208@gmail.com>
2026-09-22 21:14:09 +0530

    Add files via upload

---
+++ Q-88.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            str[i] = '-';
+        }
+    }
+
+    printf("Result: %s", str);
+
+    return 0;
+}
